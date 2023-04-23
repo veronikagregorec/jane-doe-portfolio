1eaf993ed220cd2d78a7bdbadd9e3cee938e1277
veronikagregorec <gregorec.veronika@gmail.com>
2023-04-23 13:04:58 +0200

    Added event listener

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,9 @@
+// menu
 const menuBtn = document.querySelector(".menu-btn");
 const hamburger = document.querySelector(".menu-btn__burger");
 const nav = document.querySelector(".nav");
 const menuNav = document.querySelector(".menu-nav");
 
-/** array of elements */
 const navItems = document.querySelectorAll(".menu-nav__item");
 
 let showMenu = false;
@@ -28,6 +28,16 @@ function toggleMenu() {
   }
 }
 
+nav.addEventListener("click", function () {
+  hamburger.classList.remove("open");
+  nav.classList.remove("open");
+  menuNav.classList.remove("open");
+  navItems.forEach((item) => item.classList.remove("open"));
+
+  showMenu = false;
+});
+
+// form
 const nameInput = document.querySelector("#name");
 const email = document.querySelector("#email");
 const message = document.querySelector("#message");
@@ -77,3 +87,28 @@ function emailIsvalid(email) {
   let pattern = /\S+@\S+\.\S+/;
   return pattern.test(email);
 }
+
+// scroll to top
+const scrollBtn = document.querySelector(".scroll-to-top");
+const scrollFooter = document.querySelector("footer");
+
+const refreshButton = () => {
+  if (document.documentElement.scrollTop <= 150) {
+    scrollBtn.style.display = "none";
+    scrollFooter.style.display = "none";
+  } else {
+    scrollBtn.style.display = "block";
+    scrollFooter.style.display = "block";
+  }
+};
+
+refreshButton();
+
+scrollBtn.addEventListener("click", () => {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+});
+
+document.addEventListener("scroll", () => {
+  refreshButton();
+});
